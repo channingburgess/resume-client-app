d4505006d61d5155aec8b9ff237417e0637fb522
channing.burgess <channingmburgess@gmail.com>
2024-10-18 12:45:23 -0400

    revised resume - 10 18 2024

--- client-app/src/app/landing/index/index.component.ts
+++ client-app/src/app/landing/index/index.component.ts
@@ -14,19 +14,52 @@ export class IndexComponent implements OnInit {
   ngOnInit(): void {
     this.WorkHistory = [
       {
-        company: 'CACI International Inc.',
-        position: 'Software Developer',
-        startDate: new Date("2020-03-01"),
+        company: 'Meridian Cooperative.',
+        position: 'Software Developer III',
+        startDate: new Date("2021-05-01"),
         endDate: null,
-        img: 'https://media-exp1.licdn.com/dms/image/C560BAQG7cpEvEbnoKw/company-logo_100_100/0?e=1596672000&v=beta&t=A28zawdRz3_-fNmhVMS37boECm5c7TOoYBnTxbbjIFU',
-        description: 'Working with a dedicated team to develop and support applications for the Executive Office of the President.'
+        img: '/assets/pictures/MeridianCoop.PNG',
+        description: `I develop and support payment and billing software for
+ electric, water, and fiber utilities. My work spans all
+ application layers, including POS devices, customer
+facing UI, application servers, and database
+ programming. I utilize .NET and AngularJS for
+ development, while employing PL/SQL and T-SQL for
+ database management.`
+      },
+      {
+        company: 'Cosaint Cyber.',
+        position: 'Systems Engineer II',
+        startDate: new Date("2020-09-01"),
+        endDate: null,
+        img: '/assets/pictures/CosaintCyber.PNG',
+        description: ` I implement Elasticsearch, Kibana, and Logstash
+ configuration files, set up data pipelines for various audit
+ log types (Syslog, Web, etc.) and create custom python
+ and shell scripts to collect and aggregate various types of
+ external reporting (JSON/XML). Identify, design, and
+ implement process improvements for log collection and
+ aggregation: automating manual processes, optimizing
+ data delivery, redesigning infrastructure for greater
+ scalability..`
+      },
+      {
+        company: 'CACI International Inc.',
+        position: 'Software Developer III',
+        startDate: new Date("2020-03-01"),
+        endDate: new Date("2021-05-01"),
+        img: '/assets/pictures/CACI.PNG',
+        description: `Working with a dedicated team to develop and support
+ applications for the Executive Office of the President
+ using Angular JS, C#, ASP.NET MVC, Entity Framework,
+ and T-SQL.`
       },
       {
         company: 'ACR - American College of Radiology',
         position: 'Mid Application Engineer',
         startDate: new Date("2019-08-01"),
         endDate: new Date("2020-03-01"),
-        img: 'https://media-exp1.licdn.com/dms/image/C510BAQH6-kU3CvEPYw/company-logo_100_100/0?e=1596672000&v=beta&t=8ebFmtsD71dsCma-8_d-fXCynEWuSMzCiLDL5ddClJY',
+        img: null,
         description: 'Full stack application engineer working with Angular 8, Docker, SQL, .Net Core, and REST Web APIs. I worked with a small agile team using the latest and greatest frameworks and tools to increase efficiency of research conducted within the medical field.'
       },
       {
@@ -34,7 +67,7 @@ export class IndexComponent implements OnInit {
         position: 'Application Developer II',
         startDate: new Date("2019-08-01"),
         endDate: new Date("2016-07-01"),
-        img: 'https://media-exp1.licdn.com/dms/image/C560BAQGsMCXl8Rf8XQ/company-logo_100_100/0?e=1596672000&v=beta&t=yJ2i0-10huP1Xfadxs25tjGTiU-Udfgjj4htOUH7sTg',
+        img: null,
         description: 'Full stack developer and Scrum Master. I worked on a rewrite of a large enterprise system into a modern framework. We used AngularJS, Javascript, C# MVVM, and PLSQL to develop a responsive, scalable application. As a scrum master I mitigated roadblocks to deliver successful burn down reports to stakeholders'
       },
       {
@@ -42,7 +75,7 @@ export class IndexComponent implements OnInit {
         position: 'Software Engineer Intern',
         startDate: new Date("2016-07-01"),
         endDate: new Date("2015-03-01"),
-        img: 'https://media-exp1.licdn.com/dms/image/C560BAQGmUQnhOosU6A/company-logo_100_100/0?e=1596672000&v=beta&t=pSMwG9Q65vS1Dq2jSIX4wgopZVc29LbolBT-B-Vhc2Q',
+        img: null,
         description: 'Developed in house programs to control and manage factory operations. Levered ASP.NET web forms applications as well as VBA macros to complete data change control of factory production information.'
       }];
   }
